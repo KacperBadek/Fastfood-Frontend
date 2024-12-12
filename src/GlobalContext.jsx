efe6e91a739b6a07a83f6d3f6edcc028b7ddbb56
KacperBadek <k.badek2001@gmail.com>
2024-12-12 19:25:05 +0100

    Created initial components

---
+++ src/GlobalContext.jsx
@@ -0,0 +1,9 @@
+import {createContext} from "react";
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = ({children}) => {
+
+    return <GlobalContext.Provider value={children}>{children}</GlobalContext.Provider>;
+
+}
