8de1d4111200dad711b39dc4153a31d58eac34d8
KacperBadek <k.badek2001@gmail.com>
2025-01-04 21:57:50 +0100

    Product price in ProdcutModal is now dynamic

--- src/components/modals/ProductModal.jsx
+++ src/components/modals/ProductModal.jsx
@@ -1,4 +1,4 @@
-import {useState, useContext, useCallback} from "react";
+import {useState, useContext, useCallback, useEffect} from "react";
 import ProductDetailsModal from "./ProductDetailsModal.jsx";
 import ProductPersonalizationModal from "./ProductPersonalizationModal.jsx";
 import Modal from "./Modal.jsx";
@@ -12,6 +12,7 @@ export default function ProductModal({product, toggleModal}) {
     const [personalizedAddOns, setPersonalizedAddOns] = useState([]);
     const [detailsModal, setDetailsModal] = useState(false);
     const [personalizationModal, setPersonalizationModal] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(product.price);
 
     const toggleDetailsModal = useCallback(() => {
         setDetailsModal((prev) => !prev);
@@ -34,13 +35,18 @@ export default function ProductModal({product, toggleModal}) {
         }
     };
 
+    useEffect(() => {
+        const updatedPrice = calculateTotalPrice(product.price, personalizedAddOns, quantity);
+        setTotalPrice(updatedPrice);
+    }, [quantity, personalizedAddOns]);
+
     return (
         <>
             <Modal toggle={toggleModal}>
                 <img src={product.image} alt="photo"/>
                 <h2>{product.name}</h2>
                 <p>{product.description}</p>
-                <p>Price: {product.price}$</p>
+                <p>Price: {totalPrice.toFixed(2)}$</p>
                 <input type="number" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))}/>
                 <button onClick={handleAddToOrder}>Add</button>
                 <button onClick={togglePersonalization}>Personalise</button>
